402edde32a1dd6f0162399640bd6c18acd368261
EdinPriqtel <vbarzin@gmail.com>
2016-03-15 00:34:47 +0200

    solution backbone + references

---
+++ ViewModel/ViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ViewModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using DatabaseLayer;
+
+    public class ViewModel
+    {
+    }
+}
